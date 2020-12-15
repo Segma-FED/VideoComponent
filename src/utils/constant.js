06e02d44decf1023855992e94f6962ea32a90e47
Lucas <825816425@qq.com>
2020-12-15 11:53:55 +0800

    feat(注释): 注释对齐

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -6,12 +6,12 @@
  * 播放器状态
  * */
 export const playStatus = {
-    "UNINITIALIZED": "UNINITIALIZED",         // 尚未初始化
-    "INITIALIZED": "INITIALIZED",           // 已经初始未播放
+    "UNINITIALIZED": "UNINITIALIZED",   // 尚未初始化
+    "INITIALIZED": "INITIALIZED",       // 已经初始未播放
     "ON_PLAY": "ON_PLAY",               // 正常播放
-    "PAUSED": "PAUSED",                // 正常暂停
+    "PAUSED": "PAUSED",                 // 正常暂停
     "LOADING": "LOADING",               // 加载中
-    "ERROR": "ERROR"               // 错误状态
+    "ERROR": "ERROR"                    // 错误状态
 };
 
 /**
@@ -19,5 +19,5 @@ export const playStatus = {
  * */
 export const playErrors = {
     "PLAY_FAIL": "PLAY_FAIL",             // 播放失败
-    "UNKNOWN": "UNKNOWN"                // 未知错误
+    "UNKNOWN": "UNKNOWN"                  // 未知错误
 };
